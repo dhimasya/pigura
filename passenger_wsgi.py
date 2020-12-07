4aede4f3d7a6d0f722f0cd44bb853a0f00cdde71
dhimas yudangga <dhimas@portcities.net>
2020-12-08 00:06:56 +0700

    [IMP] finalize wsgi code

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,19 +1,16 @@
-#!/usr/bin/env python3
-# Passenger WSGI configuration
-# 
-# change the path if needed
-#import sys
-#
-#path = '/hosting/python/app/dir
-#if path not in sys.path:
-#    sys.path.append(path)
+import imp
+import os
+import sys
+
+
+sys.path.insert(0, os.path.dirname(__file__))
 
 import odoo
 
 #----------------------------------------------------------
 # Common
 #----------------------------------------------------------
-odoo.multi_process = True # Nah!
+# odoo.multi_process = True # Nah!
 
 # Equivalent of --load command-line option
 odoo.conf.server_wide_modules = ['base', 'web']
@@ -25,11 +22,11 @@ conf = odoo.tools.config
 # conf['addons_path'] = '../../addons/trunk,../../web/trunk/addons'
 
 # Optional database config if not using local socket
-#conf['db_name'] = 'mycompany'
-#conf['db_host'] = 'localhost'
-#conf['db_user'] = 'foo'
-#conf['db_port'] = 5432
-#conf['db_password'] = 'secret'
+conf['db_host'] = 'localhost'
+conf['db_name'] = 'name'
+conf['db_user'] = 'user'
+conf['db_port'] = 5432
+conf['db_password'] = 'pass'
 
 #----------------------------------------------------------
 # Generic WSGI handlers application
